9f697cce110ff4e0446b5f8e2ab34e005146e4dc
rochactf <eduardo.rs@estudante.iftm.edu.br>
2024-12-02 20:42:57 -0300

    1 semestre

---
+++ script.js
@@ -0,0 +1,8 @@
+$(windows).load(function() {
+  $('.container').find('img').each(function() {
+    var imgClass = (this.width / this.height > 1) ? 'wide' : 'tall';
+    $(this).addClass(imgClass);
+
+  })
+
+})
